05953af0ff205985bef5e933157b34eb84677ae3
hzchendawei <hzchendawei@corp.netease.com>
2018-08-28 15:57:31 +0800

    misc#搭建

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # cloud-demo
 spring-cloud学习demo
+
+
+
+registration-center-web注册中心
+测试环境构建脚本
+mvn clean package  -Ptest -Dserver.port=8882
+执行
+java -jar registration-center-web-exec.jar
+
+或者执行时候指定端口  java -jar 打包之后的SpringBoot.jar  --server.port=8000
+
+
+
---
+++ provider-web/src/main/java/com/dv/controller/HiController.java
@@ -0,0 +1,20 @@
+package com.dv.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hzchendawei on 2018/8/28.
+ */
+@RestController
+public class HiController {
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
