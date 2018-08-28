ab1a64c35e632c267dc5ceb1fa596d9a7074022a
hzchendawei <hzchendawei@corp.netease.com>
2018-08-28 22:10:40 +0800

    misc#搭建 zuul

---
+++ service-zuul/src/main/java/com/dv/filter/TokenFilter.java
@@ -0,0 +1,45 @@
+package com.dv.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hzzhuqi1 on 2017/6/2.
+ */
+public class TokenFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(TokenFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+      /*  RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        logger.info("send {} request to {}", request.getMethod(), request.getRequestURI());
+        String token = request.getParameter("token");
+        if (token == null) {
+            logger.warn("token is null");
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(401);
+            return null;
+        }
+        logger.info("ok");
+        return null;*/
+        return null;
+    }
+}
